41ea5638aea9896afc27413d216cf4090e3040a8
Samir201514 <arafat.rahman3926@gmail.com>
2026-07-12 23:47:34 +0600

    Started JavaScript Coding

---
+++ Notes/JavaScript/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+let x;
+console.log(x);
+
+name = null; //with let object; without string
+console.log(typeof name);
+
+name= "Samir";
+console.log(typeof name);
+
+
+document.getElementById("sp").textContent = "MD. ARAFAT RAHMAN SAMIR"; //HTML
+document.getElementById("sp").style.backgroundColor = "red"; //CSS
