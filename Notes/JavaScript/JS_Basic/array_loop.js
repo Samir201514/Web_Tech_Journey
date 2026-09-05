58a32a946d88be3f399e2066379a8e724c3e48d4
Samir201514 <arafat.rahman3926@gmail.com>
2026-09-05 12:06:41 +0600

    added array length & splice function

--- Notes/JavaScript/JS_Basic/array_loop.js
+++ Notes/JavaScript/JS_Basic/array_loop.js
@@ -1,6 +1,9 @@
 //Arrays & Loop
 let arr = ["meow", 3.62, { id: "23-55488-3" }];
 
+console.log("Length of the array is : ", arr.length);
+arr.splice(0, 1, "Samir") // (Index No, Remove How Many, Elements) 
+
 // Normal function
 arr.forEach(function(a) {
     console.log(a);
@@ -10,8 +13,6 @@ arr.forEach(function(a) {
 arr.push({ name: "Samir" });   // Add object at the end
 arr.unshift("Hi");             // Add at the beginning
 
-console.log("Length of array is:", arr.length);
-
 // Arrow function
 arr.forEach((a) => {
     console.log(a);
@@ -20,6 +21,8 @@ arr.forEach((a) => {
 // Remove
 arr.pop();     // Removes the last element
 arr.shift();   // Removes the first element
+
+
 
 // Print indexes
 for (let index in arr) {
