580c6b32d83764ea2407392b81a05722866c21d9
Samir201514 <arafat.rahman3926@gmail.com>
2026-09-05 12:09:53 +0600

    rename the file & written js object code

---
+++ Notes/JavaScript/JS_Basic/object.js
@@ -0,0 +1,37 @@
+//Object : An object is a collection of properties and methods.
+//Each on called Entry with Key-Value pairs
+const student = {
+    name : "Samir",
+    id : "23-55488-3",
+    lang : "JavaScript",
+
+    courseName : function() {
+        console.log("Web Tech");
+    }
+}
+
+console.log(typeof student);
+
+student.name = {
+    firstName : "arafat",
+    lastName : "samir"
+}; //Modify
+console.log(student.name.lastName);
+
+
+student.cgpa = 3.62; //Insert
+console.log(student["id"]);
+
+
+delete student.lang; //Delete
+
+
+student.courseName();
+
+
+console.log(student);
+
+
+console.log(Object.entries(student));
+console.log(Object.keys(student));
+console.log(Object.values(student));
