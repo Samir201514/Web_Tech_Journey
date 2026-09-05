4baa51decf4b885238dea91c02141478ec27cd80
Samir201514 <arafat.rahman3926@gmail.com>
2026-09-05 12:08:40 +0600

    written some string function

--- Notes/JavaScript/JS_Basic/string.js
+++ Notes/JavaScript/JS_Basic/string.js
@@ -1,3 +1,17 @@
+let str = "Hello World";
+
+console.log(str.length);
+
+console.log(str.charAt(2));
+console.log(str.indexOf("e"));
+
+console.log(str.split(" "));
+
+console.log(str.includes("el"));
+
+console.log(str.slice(-4, -2)); // Support Negative
+console.log(str.substring(2, 4)); // Dosen't Support Negative
 
 
-//String
+console.log(str.replace("l", "^")); // First one
+console.log(str.replaceAll("l", "^")); // all
